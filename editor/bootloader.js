9ee8c40e858bdfde95fb2d31777c769afc914268
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-29 08:43:04 -0400

    Update bootloader.js

--- editor/bootloader.js
+++ editor/bootloader.js
@@ -1,8 +1,9 @@
 window.app = {};
 app.STATE = {};
 app.VERSION = '1.0.0';
+app.IS_MINIFIED_RELEASE = false;
 app.CURRENT_SESSION = null;
-app.EDITOR_VERSION = '1';
+app.EDITOR_VERSION = '11';
 app.REQUEST_CLIENT='1mmdeje93oa8745t4jtg4jj4o4045utj0945h49432j3';
 app.EDITOR = null;
 app.APP_NAME='Python Interpreter';
@@ -32,45 +33,64 @@ app.HANDLE_ERROR = function(error,arg) {
     };
     return returned
 };
-app.loadResource = function(t,src){
+app.loadResource = function(t,src,loaded,errored){
     if (t=='SCRIPT'){
         var v = document.createElement('script');
+        v.onload = function() {
+            loaded && loaded()
+        };
+        v.onerror = function(a){
+            errored && errored(a)
+        };
         v.setAttribute('src',src);
         v.setAttribute('data-client',app.REQUEST_CLIENT);
         document.head.appendChild(v);
     };
     if (t=='STYLESHEET'){
         var v = document.createElement('link');
-        v.setAttribute('rel', 'stylesheet')
+        v.setAttribute('rel', 'stylesheet');
+        v.onload = function() {
+            loaded && loaded()
+        };
+        v.onerror = function(a){
+            errored && errored(a)
+        };
         v.setAttribute('href',src);
         v.setAttribute('data-client',app.REQUEST_CLIENT);
         document.head.appendChild(v);
     };
 };
+app.loadScript=function(src, loaded, errored) {
+    var d = document.createElement("script");
+    d.type = "text/javascript", d.onload = function() {
+        loaded && loaded();
+    }, d.onerror = function(a) {
+        errored && errored(a);
+    }, d.src = src, console.log("LOAD SCRIPT: " + a);
+    try {
+        document.head.appendChild(d);
+    } catch (a) {
+        errored(a);
+    };
+};
 window.onerror = function(a, b, c, d, e) {
     try {
         var f = e && e.stack ? e.stack : "",
-            g = "(" + te.VERSION + ") - " + b + " @ (" + c + ", " + d + "): " + f;
-        window.console.log("Unhandled Text Editor App Error -" + a + ": " + f), logImpression(a, "unhandled_js_error", g, void 0, void 0, !0)
+            g = "(" + app.VERSION + ") - " + b + " @ (" + c + ", " + d + "): " + f;
+        window.console.log("Unhandled Editor App Error -" + a + ": " + f), logImpression(a, "unhandled_js_error", g, void 0, void 0, !0)
     } catch (b) {
         window.console.log("*** Unhandled App Error: " + a + " - " + b)
     }
 };
 
-function loadScript(a, b, c) {
-    var d = document.createElement("script");
-    d.type = "text/javascript", d.onload = function() {
-        b && b()
-    }, d.onerror = function(a) {
-        c && c(a)
-    }, d.src = a, console.log("LOAD SCRIPT: " + a);
+function onMainAppScriptLoaded() {
+    console.log("main_app_script_loaded");
     try {
-        document.head.appendChild(d)
+        MAINAPP = app, domLoaded && MAINAPP.initAfterDomLoaded(getFileId());
     } catch (a) {
-        c(a)
-    }
-}
-maybeHandleDomLoadedOrRegister();
+        throw logImpression("main_app_bootstrap_error", "app_load", a), a;
+    };
+};
 function onAceEditorScriptLoaded() {
     console.log("ace_editor_script_loaded"), app.APP_RESOURCES.aceEditorLoaded = true, MAINAPP && domLoaded && MAINAPP.initializeEditorAfterDomLoaded()
 };
@@ -78,16 +98,16 @@ function onJsChardetScriptLoaded() {
     console.log("jschardet_script_loaded"), app.APP_RESOURCES.jschardetLoaded = true;
 };
 function loadMainAppScript() {
-    loadScript("core-2-" + app.MINOR_VERSION + app.REVISION + (app.IS_MINIFIED_RELEASE ? "-min" : "") + ".js", bindFn(onMainAppScriptLoaded), bindFn(onMainAppScriptLoadError));
+    app.loadScript("core-2-" + app.MINOR_VERSION + app.REVISION + (app.IS_MINIFIED_RELEASE ? "-min" : "") + ".js", bindFn(onMainAppScriptLoaded), bindFn(onMainAppScriptLoadError));
 };
 function loadAceEditorScript() {
-    loadScript("../lib/ace-src-min-noconflict/ace.js", bindFn(onAceEditorScriptLoaded), bindFn(onAceEditorScriptLoadError));
+    app.loadScript("../lib/ace-src-min-noconflict/ace.js", bindFn(onAceEditorScriptLoaded), bindFn(onAceEditorScriptLoadError));
 };
 function loadJsChardetScript() {
-    loadScript("../lib/jschardet3-0-0.min.js", bindFn(onJsChardetScriptLoaded), bindFn(onJsChardetScriptLoadError));
+    app.loadScript("../lib/jschardet3-0-0.min.js", bindFn(onJsChardetScriptLoaded), bindFn(onJsChardetScriptLoadError));
 };
-app.loadResource('STYLESHEET','editor.css');
-app.loadResource('SCRIPT','core.js');
+//app.loadResource('STYLESHEET','editor.css');
+//app.loadResource('SCRIPT','core.js');
 var contain = document.getElementById('app_container');
 contain.innerHTML = `
 <div id='app'>\n
