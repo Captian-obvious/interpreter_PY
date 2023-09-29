8ab83f7b3641c48e1432bc3fa6be6b9532b67299
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-09-29 08:01:24 -0400

    Update bootloader.js

--- editor/bootloader.js
+++ editor/bootloader.js
@@ -44,6 +44,95 @@ app.loadResource = function(t,src){
         document.head.appendChild(v);
     };
 };
+function execLater(a, b, c) {
+    window.setTimeout(function() {
+        a(), b && b()
+    }, c || 0)
+}
+
+function updateUiAfterDomLoadedForUnsupportedBrowser() {
+    window.console.log("Showing alternate UI for unsupported browser."), logImpression("Unsupported browser: updateUiAfterDomLoad()", "app_load"), showEl(document.getElementById("mainScrollableContent"), !1), showEl(document.getElementById("unsupportedBrowserContent"), !0)
+}
+
+function maybeUpdateUiAForAppLoadFailure(a) {
+    appLoadFailureUiShown || isSupportedBrowser && (window.console.log("Showing alternate UI for app load failure."), document && "loading" !== document.readyState && (showEl(document.getElementById("mainScrollableContent"), !1), showEl(document.getElementById("appLoadErrorContent"), !0), document.getElementById("appLoadErrorDetails").textContent = "Error details: " + a, appLoadFailureUiShown = !0, logImpression("app_load_failure", "app_load", a)))
+}
+function handleDomLoaded() {
+    try {
+        if (domLoaded) return;
+        if (domLoaded = !0, console.log("In handle Dom Loaded(). domLoaded: " + domLoaded + "; isSupportedBrowser: " + isSupportedBrowser), !adsenseScriptLoadError && adLoadedDuringInit && isSupportedBrowser || (showEl(document.getElementById("topBanner"), !1), showEl(document.getElementById("adHeading"), !1), document.getElementById("mainEditorContainer").classList.add("adjustedHeightForNoAdShown"), document.getElementById("mainScrollableContent").classList.add("adjustedHeightForNoAdShown"), document.getElementById("unsupportedBrowserContent").classList.add("adjustedHeightForNoAdShown"), document.getElementById("appLoadErrorContent").classList.add("adjustedHeightForNoAdShown"), document.getElementById("mainBackgroundSpinner").classList.add("adjustedHeightForNoAdShownBackgroundSpinner")), !isSupportedBrowser) return void updateUiAfterDomLoadedForUnsupportedBrowser();
+        if (isAppLoadFailure) return void maybeUpdateUiAForAppLoadFailure(appLoadFailureMessage);
+        document.getElementById("copyright").textContent += " v" + te.VERSION, updateUiForDevice();
+        var a = null != getFileId(),
+            b = a ? getReferringSource() : "general";
+        a && "gmail" != b && (b = "drive");
+        var c = "drive" == b,
+            d = "gmail" == b,
+            e = !(c || d || isForDirectNew);
+        document.getElementById("startPageIconImgDrive").removeAttribute("visibilityHidden"), showEl(document.getElementById("startPageIconImgGeneral"), e || isForDirectNew), showEl(document.getElementById("startPageIconImgGmail"), d), showEl(document.getElementById("startPageIconImgDrive"), c), showEl(document.getElementById("startPageGeneralDetails"), e), showEl(document.getElementById("startPageGmailDetails"), d), showEl(document.getElementById("startPageDriveDetails"), c), showEl(document.getElementById("startPageNewDetails"), isForDirectNew), document.getElementById("startPageDriveDetails").style.visibility = "", showEl(document.getElementById("startPagePrompt"), !0), document.getElementById("startPagePrompt").style.visibility = "", MAINAPP && (MAINAPP.initAfterDomLoaded(getFileId()), EDITOR && MAINAPP.initializeEditorAfterDomLoaded())
+    } catch (a) {
+        throw logImpression("handle_dom_loaded_err", "app_load", a), a
+    }
+}
+
+function showEl(a, b) {
+    a.hidden = !b
+}
+
+function isElementShown(a) {
+    return !a.hidden
+}
+
+function logImpression(a, b, c, d, e, f) {
+    var g = {};
+    null != b && (g.event_category = b, "app_load" === b && null == e), null != c && (g.event_label = c), null != d && (g.value = +d), !0 == f && (g.transport_type = "beacon"), !0 == e && (g.non_interaction = !0), 0 == Object.keys(g).length ? gtag("event", a) : gtag("event", a, g)
+}
+
+function clearUrlState() {
+    window.history.replaceState({}, "", window.location.pathname)
+}
+
+function onAceEditorScriptLoaded() {
+    console.log("ace_editor_script_loaded"), aceEditorLoaded = false, MAINAPP && domLoaded && MAINAPP.initializeEditorAfterDomLoaded()
+}
+
+function onJsChardetScriptLoaded() {
+    console.log("jschardet_script_loaded"), jschardetLoaded = true
+}
+
+function onMainAppScriptLoaded() {
+    console.log("main_app_script_loaded");
+    try {
+        MAINAPP = app, domLoaded && MAINAPP.initAfterDomLoaded(getFileId())
+    } catch (a) {
+        throw logImpression("main_app_bootstrap_error", "app_load", a), a
+    }
+}
+
+function isForDirectNewFlow() {
+    return isForDirectNew
+}
+
+function isForCreateNew() {
+    return "create" == STATE.action && !getFileId()
+}
+
+function isForOpen() {
+    return "open" == STATE.action && !!getFileId()
+}
+
+function getStateUserId() {
+    return STATE.userId
+}
+
+function hasReferringApp() {
+    var a = getReferringAppName();
+    return null != a && "" !== a
+}
+
+function clearReferringApp() {
+    STATE.referringApp = null
+}
 app.loadResource('STYLESHEET','editor.css');
 app.loadResource('SCRIPT','core.js');
 var contain = document.getElementById('app_container');
